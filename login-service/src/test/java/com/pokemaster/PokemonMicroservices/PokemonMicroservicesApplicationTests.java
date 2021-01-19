71d7c9a9a77f5093e82600dc2348e6d19d8d0c2b
Metsuryuu <qjroman@gmail.com>
2021-01-19 17:02:44 -0500

    Login boiler added

---
+++ login-service/src/test/java/com/pokemaster/PokemonMicroservices/PokemonMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemaster.PokemonMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
