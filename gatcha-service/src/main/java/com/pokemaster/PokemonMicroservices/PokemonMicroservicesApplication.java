873217d7949036fbf79fbb4fe494627fb3814d81
Metsuryuu <qjroman@gmail.com>
2021-01-20 12:41:11 -0500

    Gacha service project added

---
+++ gatcha-service/src/main/java/com/pokemaster/PokemonMicroservices/PokemonMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.pokemaster.PokemonMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonMicroservicesApplication.class, args);
+	}
+
+}
