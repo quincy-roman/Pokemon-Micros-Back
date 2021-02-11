e53e06270dce482a6445f63e512d985134e6abb1
Thomas Morgan <tmorgan1@tamu.edu>
2021-02-11 13:59:12 -0600

    transitioned to shared AWS DB, connected with Zuul, added method to add new pokemon to boxes

--- pc-service/src/main/java/com/pokemaster/repository/TeamRepository.java
+++ pc-service/src/main/java/com/pokemaster/repository/TeamRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pokemaster.model.Team;
+import com.pokemaster.model.Trainer;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Integer>{
 
+	Team findByTrainer(Trainer owner);
 }
